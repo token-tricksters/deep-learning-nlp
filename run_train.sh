106a1509769adcaa5470de2c7e607603e2a4d476
ItsNiklas <nikbau2000@gmail.com>
2023-06-23 00:22:18 +0200

    fix: No timeouts on Grete
    Correct env name, Slightly more logging

--- run_train.sh
+++ run_train.sh
@@ -14,7 +14,7 @@
 
 module load anaconda3
 module load cuda
-source activate dl-gpu # Or whatever you called your environment.
+source activate dnlp2 # Or whatever you called your environment.
 
 # Printing out some info.
 echo "Submitting job with sbatch from directory: ${SLURM_SUBMIT_DIR}"
