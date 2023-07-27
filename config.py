6737696234a59821615d2b08c3516003597ee0d3
lkaesberg <larskaesberg@yahoo.de>
2023-07-27 14:21:20 +0000

    Revert "Merge branch 'feat/bert_large' into 'main'"
    This reverts merge request !13

--- config.py
+++ config.py
@@ -186,7 +186,7 @@ class BertConfig(PretrainedConfig):
   def __init__(
     self,
     vocab_size=30522,
-    hidden_size=1024,
+    hidden_size=768,
     num_hidden_layers=12,
     num_attention_heads=12,
     intermediate_size=3072,
