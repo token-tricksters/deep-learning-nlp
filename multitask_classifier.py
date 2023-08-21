46de548de59016cfae117e26bcbc5ee5488c6766
ItsNiklas <nikbau2000@gmail.com>
2023-08-21 12:12:15 +0200

    ⚡️ (sophia): Small parameter change

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -279,8 +279,9 @@ def train_multitask(args):
     if args.optimizer == "adamw":
         optimizer = AdamW(model.parameters(), lr=lr)
     elif args.optimizer == "sophiah":
+        # TODO: Tune this further, https://github.com/Liuhong99/Sophia#hyper-parameter-tuning
         optimizer = SophiaH(
-            model.parameters(), lr=lr, eps=1e-12, rho=0.03, betas=(0.985, 0.99), weight_decay=2e-1
+            model.parameters(), lr=lr, eps=1e-12, rho=0.05, betas=(0.985, 0.99), weight_decay=2e-1
         )
     else:
         raise NotImplementedError(f"Optimizer {args.optimizer} not implemented")
