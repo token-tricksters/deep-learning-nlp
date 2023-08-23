fc77f4ebd853fdf4ae1ef55ef12736ccd3d42f1d
ItsNiklas <nikbau2000@gmail.com>
2023-08-23 11:07:03 +0200

    🧑‍💻 (install): Made install faster using the libmamba solver

--- setup.sh
+++ setup.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+# First, we need a better solver for conda
+conda install -n base conda-libmamba-solver
+conda config --set solver libmamba
+
+# Create the environment
 conda create -y -n dnlp2 python=3.10
 conda activate dnlp2
 
+# Install the packages
 conda install -y pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install tqdm
 pip install requests
@@ -14,13 +20,13 @@ pip install scikit-learn
 pip install tokenizers
 pip install explainaboard_client
 pip install tensorboard
-conda install -c conda-forge spacy
-conda install -c conda-forge cupy
-conda install -c conda-forge spacy-transformers
-# packages only available via pip
+
+# POS and NER tagging
+conda install -y -c conda-forge spacy cupy spacy-transformers
 pip install spacy-lookups-data
 python -m spacy download en_core_web_sm
 
+# For the pre-commit hook
 pip install pre-commit
 
 # Make pip happy for triton
