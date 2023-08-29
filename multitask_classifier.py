eae0922afbfacc08611178b48c7af56f8f444ccc
ItsNiklas <nikbau2000@gmail.com>
2023-08-29 20:58:37 +0200

    🚑️ fix execution on cluster

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -744,12 +744,12 @@ if __name__ == "__main__":
         # Search Algorithm: Optuna
         algo = OptunaSearch(metric=["sst_dev_acc", "para_dev_acc", "sts_dev_acc"], mode=["max"] * 3)
 
-        ray.init(num_cpus=4, log_to_driver=False)  # Don't print logs to console
+        ray.init(num_cpus=8, log_to_driver=False)  # Don't print logs to console
 
         tuner = tune.Tuner(
             tune.with_resources(
                 tune.with_parameters(train_multitask),
-                resources={"cpu": 4, "gpu": torch.cuda.device_count()},
+                resources={"cpu": 4, "gpu": 1},
             ),
             tune_config=tune.TuneConfig(
                 num_samples=args.hpo_trials,  # Number of trials
