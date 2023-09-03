a8ffe43478fdb96b74ac3201f88918e66b7b5693
ItsNiklas <nikbau2000@gmail.com>
2023-09-03 11:52:09 +0200

    🔨 (setup): Finalize setup.sh
    Fixed some versions, optimized process

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash -l
 
 set -e
 
@@ -10,30 +10,20 @@ conda config --set solver libmamba
 conda create -y -n dnlp2 python=3.10
 conda activate dnlp2
 
+# Make pip happy for triton
+pip install cmake lit
+
 # Install the packages
-conda install -y pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
-pip install tqdm
-pip install requests
-pip install importlib-metadata
-pip install filelock
-pip install scikit-learn
-pip install tokenizers
-pip install explainaboard_client
-pip install tensorboard
-pip install torch_tb_profiler
+conda install -y pytorch=2.0 torchvision=0.15 torchaudio=2.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install "tqdm>=4.66" requests importlib-metadata "filelock>=3.12" "scikit-learn>=1.3" "tokenizers>=0.13" "explainaboard_client>=0.1" "tensorboard>=2.14" "torch_tb_profiler>=0.4" "pytorch_optimizer>=2.11"
 
 # POS and NER tagging
-conda install -y -c conda-forge spacy cupy spacy-transformers
-pip install spacy-lookups-data
+conda install -y -c conda-forge "spacy>=3.5" cupy spacy-transformers
+pip install "spacy-lookups-data>=1.0"
 python -m spacy download en_core_web_sm
 
 # For the pre-commit hook
 pip install pre-commit
 
-# Make pip happy for triton
-pip install cmake
-pip install lit
-
 # For the hyperparameter tuning
-pip install "ray[tune]"
-pip install optuna
+pip install "ray[tune]>=2.6" "optuna>=3.3"
